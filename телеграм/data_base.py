92538140040a29a8c88cb9e0e91aaaac01586e88
Nibani <78642250+Nibani@users.noreply.github.com>
2022-11-24 22:02:28 +0300

    Add files via upload

--- телеграм/data_base.py
+++ телеграм/data_base.py
@@ -15,19 +15,19 @@ with con:
             cardnumber TEXT NOT NULL,
             balance TEXT NOT NULL,
             cvc TEXT NOT NULL,
+            cardvalidity TEXT NOT NULL,
             cardstatus TEXT NOT NULL,
             transmissionhistory TEXT,
             purchaseshistory TEXT
         );
     """)
-sql = '''INSERT or REPLACE INTO users (id, name, phonenumber, cardnumber, balance, cvc, cardstatus, transmissionhistory, purchaseshistory) 
-                                       values(?, ?, ?, ?, ?, ?, ?, ?, ?)'''
+sql = '''INSERT or REPLACE INTO users (id, name, phonenumber, cardnumber, balance, cvc, cardvalidity, cardstatus, transmissionhistory, purchaseshistory) 
+                                       values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'''
 
-data = [(1, 'Равиль', '89998887733', '1234123412341234', '1023', '020', 'АКТИВНА', '', '[(Ашан капуста 999)]'),
-       (2, 'Ксения', '87776665544', '5678567856785678', '2020', '202', 'ЗАБЛОКИРОВАНА', '[(Ксения Владислав 100), (Равиль Ксения 50)]', '')]
+data = [(1, 'Равиль', '89998887733', '1234123412341234', '1023', '020', 'АКТИВНА', '05,29','','[(Ашан капуста 999)]'),
+       (2, 'Ксения', '87776665544', '5678567856785678', '2020', '202', '07,28','ЗАБЛОКИРОВАНА', '[(Ксения Владислав 100), (Равиль Ксения 50)]', '')]
 with con:
     con.executemany(sql, data)
 
 cur.execute("SELECT * FROM users;")
 all_users = cur.fetchall()
-print(all_users)
--- телеграм/main.py
+++ телеграм/main.py
@@ -1,15 +1,51 @@
 import telebot
+from telebot import types
+import data_base
 
-token = '5886592720:AAGsMJgoWTs6rZhEPAmpQLZtWG1tqOfXbv4'
-Bot = telebot.TeleBot(token)
+Bot = telebot.TeleBot('5886592720:AAGsMJgoWTs6rZhEPAmpQLZtWG1tqOfXbv4')
+user_index = 0
+
+@Bot.message_handler(commands=['start','help'])
+def login_1(m, res=True):
+    Bot.reply_to(m, 'Здравствуйте, я текстовый помощник NeoBank! Введите ваш номер телефона для'
+    + ' дальнейших действий')
+
+@Bot.message_handler(func=lambda m: True)
+def login_2(message):
+    login_check = 0
+    for i in range(len(data_base.all_users)):
+        if data_base.all_users[i][2] == message.text:
+            login_check += 1
+            user_index = i
+            Bot.reply_to(message, 'Введите cvc код для подтверждения, что вы владелец карты')
+            break
+    if login_check == 0:
+        Bot.reply_to(message, 'Неверно введённый номер телефона')
+#def login_3(message): ЗАВТРА ДОДЕЛАТЬ!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    
 
 
 @Bot.message_handler(func=lambda m: True)
-def get_text_messages(message):
-  if message.text == "Привет" or message.text == '/start':
-      Bot.reply_to(message, "Привет, сейчас я твой консультант.")
-  elif message.text == "/help":
-      Bot.reply_to(message, "Напиши Привет")
-  else:
-      Bot.reply_to(message, "Я тебя не понимаю. Напиши /help.")
-Bot.infinity_polling()
+def functions(m, res=False):
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+    first_item = types.KeyboardButton('Баланс')
+    second_item = types.KeyboardButton('Анализ финансов')
+    third_item = types.KeyboardButton('Что-то с банком, я хз')
+    markup.add(first_item)
+    markup.add(second_item)
+    markup.add(third_item)
+    Bot.send_message(m.chat.id, 'Вы успешно вошли. Выберите, что вы хотите сделать', reply_markup=markup)
+
+@Bot.message_handler(content_types=['text'])
+def message_text(message):
+    answer = ''
+    if message.text.strip()=='Баланс' :
+        answer = 'Ты бомж'
+    elif message.text.strip()=='Анализ финансов' :
+        answer = 'Тебе пора на трассу'
+    elif message.text.strip()=='Что-то с банком, я хз' :
+        answer = 'Я реально хз'
+
+    Bot.send_message(message.chat.id, answer)
+
+Bot.polling(non_stop=True, interval=0)
